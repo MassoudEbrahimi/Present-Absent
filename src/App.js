8cb8fcf52c1be41e7a996ecf9e7378602cd26ba6
Masoud Ebrahimi <ebrahimi@barsamrayan.local>
2020-06-23 10:00:59 +0430

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.css';
-
+import $ from 'jquery'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min"
 import "@mdi/font/css/materialdesignicons.css"
@@ -23,6 +23,7 @@ class App extends Component {
       time: new Date(),
       date: new Date().toLocaleDateString("fa-IR"),
       lastPersonStatus: "",
+      lastTimeChecked: "",
 
     }
   }
@@ -32,6 +33,13 @@ class App extends Component {
       1000)
     let stdate = new Date().toLocaleDateString("fa-IR")
     const res = await getData({ stdate })
+    $("input[type=text]").focus()
+    $('.input').keypress(function (e) {
+      if (e.which === 13) {
+        $('form#login').submit();
+        return false;    //<---- Add this line
+      }
+    });
     // const result = res.data.map((o, i) => {
     //   for (var element in o) {
     //     if (o[element] === 1) {
@@ -55,25 +63,26 @@ class App extends Component {
   CreatetitleName = (e) => {
 
     const { Users } = this.state
-    let res, code, status
+    let res, code, status, time
     Users.forEach((node, index) => {
       if (node.code === e.target.value) {
         res = node.name
         code = node.code
         status = node.laststatus
+        time = node.Lasttime
       }
     })
     console.log(res)
-    this.setState({ titleName: res ? res : "", inputCode: code ? code : null, lastPersonStatus: status })
+    this.setState({ titleName: res ? res : "", inputCode: code ? code : null, lastPersonStatus: status, lastTimeChecked: time })
     console.log(res ? res : null)
   }
   Login = async (e) => {
     e.preventDefault()
-    const { Users, TypeOperation, titleName, code } = this.state
+    const { Users, TypeOperation, titleName, inputCode } = this.state
     let Single;
     Users.forEach((node, index) => {
-      if (node.name === titleName)
-        if (node.laststatus === true)
+      if (node.name === titleName) {
+        if (node.laststatus === true) {
           Single = {
             id: node.id,
             code: node.code,
@@ -85,20 +94,22 @@ class App extends Component {
             // isLogin: node.isLogin,
             // isOut: node.isOut
           }
-      if (node.laststatus === false)
-        Single = {
-          id: node.id,
-          code: node.code,
-          // name: node.name,
-          time: new Date().toLocaleTimeString("fa-IR"),
-          LastStatus: false
-          // Timeto: null,
-          // strdate: new Date().toLocaleDateString("fa-IR"),
-          // isLogin: node.isLogin,
-          // isOut: node.isOut
         }
+        if (node.laststatus === false) {
+          Single = {
+            id: node.id,
+            code: node.code,
+            // name: node.name,
+            time: new Date().toLocaleTimeString("fa-IR"),
+            LastStatus: false
+            // Timeto: null,
+            // strdate: new Date().toLocaleDateString("fa-IR"),
+            // isLogin: node.isLogin,
+            // isOut: node.isOut
+          }
+        }
+      }
     })
-
     debugger
     if (titleName === "") {
       Notification.fire(
@@ -109,7 +120,7 @@ class App extends Component {
       )
     }
     else
-      if (code === null) {
+      if (inputCode === null) {
         Notification.fire(
           {
             icon: 'warning',
@@ -128,7 +139,10 @@ class App extends Component {
                 title: 'ثبت ساعت کاری با موفقیت انجام شد'
               }
             )
-            this.setState({ titleName: "", inputCode: "" })
+            this.setState({ titleName: "", inputCode: "", lastTimeChecked: "" })
+            setTimeout(function () {
+              window.location.reload();  //Refresh page
+            }, 1000);
           }
         }
         catch (ex) {
@@ -201,7 +215,7 @@ class App extends Component {
     return res === undefined ? "btn btn-outline-primary col-xl-4 col-md-4 col-sm-4 m-4" : res
   }
   render() {
-    const { titleName, Users, lastTime, lastPersonStatus, inputCode } = this.state
+    const { titleName, Users, lastTimeChecked, lastPersonStatus, inputCode } = this.state
     if (Users === null) return ''
     return (
       <div>
@@ -219,16 +233,17 @@ class App extends Component {
           </div>
           <div className="col-lg-6 col-md-6 login-box">
             <div className="col-lg-10 login-title  mx-auto">
-              <input autoComplete="off" maxLength={6} minLength={2} value={inputCode} id="input" type="text" onChange={this.CreatetitleName} className="input form-control-lg text-center no-outline col-7" style={{ fontSize: "45px" }} />
+              <input id="inputField" autoComplete="off" maxLength={6} minLength={2} value={inputCode} id="input" type="text" onChange={this.CreatetitleName} className="input form-control-lg text-center no-outline col-7" style={{ fontSize: "45px" }} />
               <label className="form-control-label col-4" >:کد کاربری</label>
             </div>
             <span className="text-light" style={{ fontSize: "25px" }}>{titleName}</span>
             <div className="col-lg-12 login-form mt-2">
               <div className="col-lg-12 login-form ">
-                <form onSubmit={this.Login} method="get" action="">
+                <form onSubmit={this.Login} method="get" action="" id="login">
                   <div className="form-group mx-auto col-12 text-center  mt-4 ">
 
                     <button
+                      id="buttonSubmit"
                       aria-disabled="true"
                       type="submit"
                       onClick={this.Exit}
@@ -244,7 +259,7 @@ class App extends Component {
                       style={{ fontSize: "35px" }}>
                       ورود
                     </button> */}
-                    <p className="text-light" style={{ fontSize: "20px" }} dir="rtl"> آخرین ثبت ساعت  :<span style={{ fontSize: "20px" }} className="ml-2 mr-2">{lastTime}</span></p>
+                    <p className="text-light" style={{ fontSize: "20px" }} dir="rtl"> آخرین ثبت ساعت  :<span style={{ fontSize: "20px" }} className="ml-2 mr-2">{lastTimeChecked}</span></p>
                   </div>
                 </form>
               </div>
